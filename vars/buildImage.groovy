4272e8cc031f92467b6370b562fc5b1eacc4269a
unknown <mehdi.birouk95@gmail.com>
2023-08-02 10:57:46 +0100

    config shared library to accept params

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imgName) {
     echo "Building the docker image"
 
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh 'docker build -t 123456789mehdibirouk/demo-app:jma-2.0 .'
+        sh "ocker build -t $imgName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push 123456789mehdibirouk/demo-app:jma-2.0'
+        sh "docker push $imgName"
     }
 }
